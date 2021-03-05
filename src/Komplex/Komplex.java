cac259c309f12172e1a6cb0271b12409944645ce
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-03-05 11:18:20 +0100

    5/3-2021

--- src/Komplex/Komplex.java
+++ src/Komplex/Komplex.java
@@ -1,5 +1,25 @@
 package Komplex;
 
 public class Komplex {
-    double i;
+    double real, img;
+
+    Komplex(double r, double i){
+        this.real = r;
+        this.img = i;
+    }
+    public static Komplex sum(Komplex c1, Komplex c2)
+    {
+        Komplex temp = new Komplex(0, 0);
+
+        temp.real = c1.real + c2.real;
+        temp.img = c1.img + c2.img;
+
+        return temp;
+    }
+    public static void main(String args[]) {
+        Komplex c1 = new Komplex(5.5, 4);
+        Komplex c2 = new Komplex(1.2, 3.5);
+        Komplex temp = sum(c1, c2);
+        System.out.printf("Sum is: " + temp.real + " + " + temp.img + "i");
+    }
 }
